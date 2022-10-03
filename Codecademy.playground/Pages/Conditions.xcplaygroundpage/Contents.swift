5086771b098baa8bdabf6d5f0fac6638be4fab5d
arthurwrls <arthurwrls@outlook.com>
2022-10-03 22:54:03 +0100

    Added conditions file

--- Codecademy.playground/Pages/Conditions.xcplaygroundpage/Contents.swift
+++ Codecademy.playground/Pages/Conditions.xcplaygroundpage/Contents.swift
@@ -1,7 +1,44 @@
-//: [Previous](@previous)
 
-import Foundation
+// if statement
+var age = 65
 
-var greeting = "Hello, playground"
+if age <= 10 {
+    print("baby")
+}
+else if age >= 10, age < 15 {
+    print("teen")
+}
+else if age >= 15, age < 20 {
+    print("student")
+}
+else if age >= 20, age < 30 {
+    print("young")
+}
+else if age >= 30, age < 40 {
+    print("family")
+}
+else if age >= 40, age < 60 {
+    print("expirienced")
+}
+else {
+    print("old")
+}
 
-//: [Next](@next)
+// switch case
+
+switch age {
+case 0...10:
+    print("baby")
+case 10...15:
+    print("teen")
+case 15...20:
+    print("student")
+case 20...30:
+    print("young")
+case 30...40:
+    print("family")
+case 40...60:
+    print("expirienced")
+default:
+    print("old")
+}
