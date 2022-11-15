b85608ee53698d87676b7ea4c24ec829da3ccf98
arthurwrls <arthurwrls@outlook.com>
2022-11-15 21:31:08 +0200

    User choice is randomised Set

---
+++ Codecademy.playground/Pages/RockPaperScissors_upgr.xcplaygroundpage/Contents.swift
@@ -0,0 +1,60 @@
+// The slightly upgraded game, where user choice is randomised Set
+
+func getUserChoice() -> String {
+    var randomNumber: Set = ["rock", "paper", "scissors"]
+    return randomNumber.randomElement()!
+}
+
+func getComputerChoice() -> String {
+    let randomNumber = Int.random(in: 0 ... 2)
+
+    switch randomNumber {
+        case 0:
+            return "rock"
+        case 1:
+            return "paper"
+        case 2:
+            return "scissors"
+        default:
+            return "Something went wrong"
+    }
+}
+
+func determineWinner(_ userChoice: String, _ compChoice: String) -> String {
+    var decision = "It's a tie"
+
+    switch userChoice {
+        case "rock":
+            if compChoice == "paper" {
+                decision = "The computer won"
+            }
+            else if compChoice == "scissors" {
+                decision = "The user won"
+            }
+
+        case "paper":
+            if compChoice == "rock" {
+                decision = "The user won"
+            }
+            else if compChoice == "scissors" {
+                decision = "The computer won"
+            }
+        case "scissors":
+            if compChoice == "rock" {
+                decision = "The computer won"
+            }
+            else if compChoice == "paper" {
+                decision = "The user won"
+            }
+        default:
+            print("Something went wrong")
+    }
+    return decision
+}
+
+let userChoice = getUserChoice()
+let compChoice = getComputerChoice()
+
+print("You threw \(userChoice)")
+print("The computer threw \(compChoice)")
+print(determineWinner(userChoice, compChoice))
